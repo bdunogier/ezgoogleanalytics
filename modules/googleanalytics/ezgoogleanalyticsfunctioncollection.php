7a2aded93bcef7907d9e080a5c093132d2297972
op <op@c2d933fd-d4e9-4671-8bc9-a370a7c7d5f5>
2009-10-28 13:54:21 +0000

    - fix an issue when node or nodeId is NULL, the url parameter is never used in this case.
    git-svn-id: http://svn.projects.ez.no/ezgoogleanalytics/trunk/extension/ezgoogleanalytics@10 c2d933fd-d4e9-4671-8bc9-a370a7c7d5f5

--- modules/googleanalytics/ezgoogleanalyticsfunctioncollection.php
+++ modules/googleanalytics/ezgoogleanalyticsfunctioncollection.php
@@ -4,46 +4,46 @@
 **/
 class eZGoogleAnalyticsFunctionCollection
 {
-	/**
-	* Function definition for googleanalytics/pagedata
-	* @param eZContentObjectTreeNode $node
-	* @param int $nodeID
-	* @param string $url
-	* @return array of pagedata
-	**/
-	public static function fetchPagedata( $node = false, $nodeID = false, $url = false )
-	{
-		if ( $node === false and $nodeID === false and $url === false)
-		{
-			eZDebug::writeError("You have to provide either node, node_id or url", __METHOD__ );
-			return array( 'result' => false );
-		}
+    /**
+    * Function definition for googleanalytics/pagedata
+    * @param eZContentObjectTreeNode $node
+    * @param int $nodeID
+    * @param string $url
+    * @return array of pagedata
+    **/
+    public static function fetchPagedata( $node = false, $nodeID = false, $url = false )
+    {
+        if ( $node === false and $nodeID === false and $url === false)
+        {
+            eZDebug::writeError("You have to provide either node, node_id or url", __METHOD__ );
+            return array( 'result' => false );
+        }
 
-		if ( $node !== false && $node instanceof eZContentObjectTreeNode )
-		{
-			$nodeID = $node->attribute( 'node_id' );
-		}
-		elseif( $nodeID !== false )
-		{
-			if ( !eZContentObjectTreeNode::fetch( $nodeID ) )
-			{
-				eZDebug::writeError( "Node $nodeID could not be fetched", __METHOD__ );
-				return array( 'result' => false );
-			}
-		}
+        if ( $node !== false && $node instanceof eZContentObjectTreeNode )
+        {
+            $nodeID = $node->attribute( 'node_id' );
+        }
+        elseif( $nodeID !== false  && $nodeID != NULL )
+        {
+            if ( !eZContentObjectTreeNode::fetch( $nodeID ) )
+            {
+                eZDebug::writeError( "Node $nodeID could not be fetched", __METHOD__ );
+                return array( 'result' => false );
+            }
+        }
 
-		$db = eZDB::instance();
-		if ( $nodeID !== false )
-		{
-			$where = "node_id = $nodeID";
-		}
-		else
-		{
-			$url = $db->escapeString( $url );
-			$where = "url LIKE '$url'";
-		}
+        $db = eZDB::instance();
+        if ( $nodeID !== false  && $nodeID != NULL )
+        {
+            $where = "node_id = $nodeID";
+        }
+        else
+        {
+            $url = $db->escapeString( $url );
+            $where = "url LIKE '$url'";
+        }
 
-		$query = <<< EOF
+        $query = <<< EOF
 SELECT SUM(aggregate.pageviews) AS pageviews,
        SUM(aggregate.uniquepageviews) AS uniquepageviews,
        SUM(aggregate.entrances) AS entrances,
@@ -56,16 +56,16 @@ FROM (
        UNION
        (SELECT * FROM ezgoogleanalytics_pagedata_incremental AS incremental WHERE $where) ) AS aggregate
 EOF;
-		$res = $db->arrayQuery( $query );
-		if ( $res == false or $res[0]['pageviews'] === null )
-		{
-			$result = false;
-		}
-		else
-		{
-			$result = $res[0];
-		}
-		return array( 'result' => $result );
-	}
+        $res = $db->arrayQuery( $query );
+        if ( $res == false or $res[0]['pageviews'] === null )
+        {
+            $result = false;
+        }
+        else
+        {
+            $result = $res[0];
+        }
+        return array( 'result' => $result );
+    }
 }
 ?>
